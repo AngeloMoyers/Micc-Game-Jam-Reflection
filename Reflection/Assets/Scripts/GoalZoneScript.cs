476b33850ce04b687cde521ada73a96ef3f3c348
ThatGuyIO <amoyers25@gmail.com>
2021-12-30 16:28:34 -0600

    Added Goal SFX, retractable wall art, level 10

--- Reflection/Assets/Scripts/GoalZoneScript.cs
+++ Reflection/Assets/Scripts/GoalZoneScript.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class GoalZoneScript : MonoBehaviour
 {
+    [Header("Logic")]
     [SerializeField] GridMovementBase m_assignedCharacter;
+
+    [Header("Audio")]
+    [SerializeField] AudioClip m_stepOn;
+    [SerializeField] AudioClip m_stepOff;
+
+    private AudioSource m_audioSource;
 
     void Start()
     {
-        
+        m_audioSource = GetComponent<AudioSource>();
     }
     
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject == m_assignedCharacter.gameObject)
         {
             m_assignedCharacter.SetAtGoal(true);
+            m_audioSource.PlayOneShot(m_stepOn);
         }
     }
 
@@ -29,6 +33,7 @@ public class GoalZoneScript : MonoBehaviour
         if (collision.gameObject == m_assignedCharacter.gameObject)
         {
             m_assignedCharacter.SetAtGoal(false);
+            m_audioSource.PlayOneShot(m_stepOff);
         }
     }
 }
